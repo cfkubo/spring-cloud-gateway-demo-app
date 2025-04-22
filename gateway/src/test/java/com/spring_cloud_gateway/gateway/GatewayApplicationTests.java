42e63ba7646044434be4ca8303d48f10c7c1c08b
cfkubo <arul.vannala@gmail.com>
2025-04-22 12:42:43 -0600

    test

---
+++ gateway/src/test/java/com/spring_cloud_gateway/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_cloud_gateway.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
